f2101e8609e65c2501e8e9a9ba6362dcd459c974
AvutE <tuva@etavo.com>
2018-09-11 09:23:02 +0200

    1 + 1/2

---
+++ AntalOrd/AntalOrd/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AntalOrd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening: ");
+            String mening = Console.ReadLine();
+
+            Regex ord = new Regex(@"(\w+|&)");
+            MatchCollection match = ord.Matches(mening);
+
+            Console.WriteLine("Antalet ord i meningen: " + match.Count);
+
+            foreach (Match antal in match)
+            {
+                Console.WriteLine(antal + " " + "(" + antal.Length + ")");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Storleksordning/Storleksordning/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Storleksordning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mening: ");
+            String mening = Console.ReadLine();
+
+            Regex antal = new Regex(@"(\w+|&)");
+            MatchCollection match = antal.Matches(mening);
+
+            foreach (Match ord in match)
+            {
+                string[] matches = new string[match.Count];
+
+                Array.Sort(matches, (x, y) => x.Length.CompareTo(y.Length));
+
+                Console.WriteLine(ord);
+            }
+
+            char[] arr = mening.ToCharArray();
+            Array.Reverse(arr);
+            String backward = new string(arr);
+
+            Console.WriteLine(backward);
+
+            Console.ReadLine();
+        }
+    }
+}
